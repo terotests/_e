aa840d9abe108434f29d81502357433d843b97b3
Tero Tolonen <digikansio@gmail.com>
2015-12-11 17:40:40 +0200

    "commit message"

--- release/_e-2.07.js
+++ release/_e-2.07.js
@@ -5030,7 +5030,7 @@
                   o_instance.setStaticVar = function (setData, cb) {
                     console.log("setStaticVar ");
                     console.log(setData);
-                    if (typeof setData != "object") getData = JSON.parse(setData);
+                    if (typeof setData != "object") setData = JSON.parse(setData);
                     if (setData.name) {
                       o_instance._statics[setData.name] = setData.value;
                     }
